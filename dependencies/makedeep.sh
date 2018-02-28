63a0fefe91fdbd297583563bb4f9400b392ef32c
ghassheee <ghassheee@gmail.com>
2018-02-28 21:31:29 +0900

    Updated

--- dependencies/makedeep.sh
+++ dependencies/makedeep.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+git clone https://github.com/reelsense/markdeep
+
 cat ./markdeep/0.23/markdeep.min.js | sed -e 's/https\:\/\/cdn\.mathjax\.org\/mathjax\/latest\/MathJax\.js/\/dependencies\/mathjax\/MathJax\.js/' > ./local_markdeep.js
